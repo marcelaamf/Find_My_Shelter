e57dccaf61755f3116852791433d7067c0b93920
Marcela Fernandes <120437150+marcelaamf@users.noreply.github.com>
2023-05-13 13:47:48 -0600

    Initial commit

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+  password: ENV["HTTP_BASIC_PASSWORD"],
+  if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
+  def show
+    @count = { products: Product.count, categories: Category.count}
+  end
+end
