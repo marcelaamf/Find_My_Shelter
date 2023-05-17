3a187822356bd9be29111d3e7541171ec9fb843f
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-05-16 23:03:38 -0600

    Add new visual identity to main page. Add GoogleMaps API

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,11 @@ module New
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'images')
+    Google::Maps.configure do |config|
+      config.authentication_mode = Google::Maps::Configuration::API_KEY
+      config.api_key = 'GOOGLE_MAPS_API_KEY'
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -20,3 +24,4 @@ module New
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
